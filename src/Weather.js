ff52d34406f7feaaef6fcb2127e378695bf4955d
LinOshiro <99516179+LinOshiro@users.noreply.github.com>
2022-05-23 12:11:55 -0700

    Added the API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Los Angeles",
-    date: "Tuesday 10:00",
-    temp: "77",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-    description: "Sunny",
-    humidity: "30%",
-    wind: "5",
-    maxTemp: "80",
-    minTemp: "64",
-    feelsLike: "77",
-  };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-  return (
+  function handleResponse(response) {
+    console.log(response.data);
+
+    setWeatherData = ({
+      ready: true,
+      date: "Tuesday 10:00",
+      temp: response.data.main.temp,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      maxTemp: "80",
+      minTemp: "64",
+      feelsLike: "77",
+      city: response.data.name,
+    });
+  }
+
+  if (weatherData.ready) {
     <div className="Weather">
       <form className="mb-3">
         <div className="row">
@@ -49,15 +58,15 @@ export default function Weather() {
         <h1>{weatherData.city}</h1>
         <ul>
           <li>Last updated: {weatherData.date}</li>
-          <li>{weatherData.description}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
         </ul>
       </div>
       <div className="row">
         <div className="col-6">
           <div className="clearfix weather-temperature">
-            <img src={weatherData.imgUrl} alt="Sunny" className="float-left" />
+            <img src={weatherData.iconUrl} alt={weatherData.description} className="float-left" />
             <span className="float-left">
-              <strong>{weatherData.temp}</strong>
+              <span className="temperature">{Math.round(weatherData.temp)}</span>
             </span>
             <span className="units"> °F</span>
           </div>
@@ -73,5 +82,12 @@ export default function Weather() {
         </div>
       </div>
     </div>
-  );
+  
+  } else {
+    const apiKey = "358bb59892afa5069bcb43f658651551";
+    let apiUrl = `http://api.opemweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+  
+    return "Loading...";
+  }
 }
