3491fa6fd0cc33f23bba5daaece144d30f959796
LinOshiro <99516179+LinOshiro@users.noreply.github.com>
2022-05-31 11:16:00 -0500

    updated from one day to five day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon.js";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay.js";
 
 export default function WeatherForecast(props) {
     const [loaded, setLoaded] = useState(false);
@@ -18,16 +19,17 @@ export default function WeatherForecast(props) {
         return (
             <div className="WeatherForecast">
                 <div className="row">
-                    <div className="col">
-                        <div className="WeatherForecast-day">Thu</div>
-                        <WeatherIcon code="01d" size={32} />
-                        <div className="WeatherForecast-temperatures">
-                            <span className="WeatherForecast-temperature-max">19°</span>
-                            <span className="WeatherForecast-temperature-min">10°</span>
-                        </div>
+                    {forecast.map(function (dailyForecast, index) {
+                        if (index < 5) {
+                            return (
+                              <div className="col">
+                                <WeatherForecastDay data={dailyForecast} />
+                              </div>
+                            );
+                        }
+                    })}
                     </div>
                 </div>
-            </div>
         );
     } else {
         let apiKey = "358bb59892afa5069bcb43f658651551";
